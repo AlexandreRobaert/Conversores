43af651a8bc86105b1bc8ddc292304915a8064d6
Alexandre Robaert <robaert.developer@icloud.com>
2021-02-27 00:39:24 -0300

    Layout finalizado com suas respectivas IBOutlets e IBActions

---
+++ Conversores/ConversorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Alexandre Robaert on 27/02/21.
+//
+
+import UIKit
+
+class ConversorViewController: UIViewController {
+
+    @IBOutlet weak var labelCategoriaUnidades: UILabel!
+    @IBOutlet weak var tfValor: UITextField!
+    @IBOutlet weak var lbResultado: UILabel!
+    @IBOutlet weak var labelUnidadeResultado: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func proximaUnidade(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func converter(_ sender: UIButton) {
+        
+    }
+    
+}
+
